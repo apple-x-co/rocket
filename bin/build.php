4cd89176c6c74b8ba745afb69b5c39acd84280cd
apple-x-co <apple.x.co@gmail.com>
2022-12-26 16:10:46 +0900

    chore: build

--- bin/build.php
+++ bin/build.php
@@ -3,8 +3,7 @@
 passthru('
 rm -rf build && mkdir build
 cp -r bin src composer.json build/ && rm build/bin/build.php &&
-composer config -d build/ platform.php 5.6 &&
-composer install -d build/ --no-dev &&
+composer install -d build/ --no-interaction --no-progress --prefer-dist --no-dev &&
 php -d phar.readonly=off ./phar-composer.phar build build/ &&
 php ./rocket.phar --info --no-color
 ', $code);
